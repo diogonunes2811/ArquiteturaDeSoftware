c46c8366a0dcbdc664ba74fbd855f3c0e8fc9ca6
user <user@hgec16060>
2017-11-01 08:15:32 -0200

    ok

--- src/br/edu/up/as/rest/PessoaRest.java
+++ src/br/edu/up/as/rest/PessoaRest.java
@@ -1,16 +1,24 @@
 package br.edu.up.as.rest;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.EntityManager;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.xml.rpc.ServiceException;
 
+import br.edu.up.as.dao.Conexao;
 import br.edu.up.as.dao.PessoaDao;
 import br.edu.up.as.entidade.Pessoa;
 import br.edu.up.as.service.PessoaService;
@@ -34,5 +42,28 @@ public class PessoaRest {
 		List<Pessoa> pessoas = new PessoaDao().listar();
 		return new ArrayList<>(pessoas);
 	}
+	
+	@PUT
+	@Produces(MediaType.APPLICATION_JSON)
+	public void editarPes(Pessoa pessoa) {
+		try {
+			new PessoaService().alterar(pessoa);
+		} catch (ServiceException e) {			
+			e.printStackTrace();
+		}	
+	}
+
+	@DELETE
+	@Produces(MediaType.APPLICATION_JSON)
+	public void excluir(Pessoa pessoa) {
+		try {
+			new PessoaService().excluir(pessoa);
+		} catch (ServiceException e) {			
+			e.printStackTrace();
+		}	
+	}
+	
+	
+
 
 }
